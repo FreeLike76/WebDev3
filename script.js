8aaa9b77cd8da7d3e634bba0270c0b404570a371
Dmytro Geleshko <dmytro.geleshko01@gmail.com>
2020-11-12 21:56:12 +0200

    day 2+ changes: -added footer -added some information cards to footer -reworked the cart on header

--- script.js
+++ script.js
@@ -3,5 +3,4 @@ let header = document.querySelector("header");
 
 window.addEventListener("scroll",function(){
     header.classList.toggle("fullHeader", window.scrollY > 400)
-    console.log("hey")
 })
