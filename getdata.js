85b44e42e6a83aa88833d9c970daac3a10ddfda6
Dmytro Geleshko <dmytro.geleshko01@gmail.com>
2020-11-19 17:27:28 +0200

    json server pls work pepeHands

--- getdata.js
+++ getdata.js
@@ -1,1 +1,10 @@
-const url = "https://my-json-server.typicode.com/FreeLike76/kpi_web_3_server"
+const server_url = "https://my-json-server.typicode.com/FreeLike76/kpi_web_3_server"
+const test_url = "https://jsonplaceholder.typicode.com/users"
+
+function sendRequest(url){
+    return fetch(url).then(response=>{
+        return response.json()
+    })
+}
+
+const productList = sendRequest(server_url)
